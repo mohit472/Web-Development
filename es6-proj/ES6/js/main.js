e07907574a21ec96c5157631a3927c64731c8d80
Surya Kant Bansal <skb1129@yahoo.com>
2017-10-08 10:37:55 +0530

    npm run build

---
+++ es6-proj/ES6/js/main.js
@@ -0,0 +1,12 @@
+"use strict";
+
+var batch = "UCA2018";
+var batch2 = "UCA2019";
+
+function getStudentDetails() {
+    document.getElementById("username").innerHTML = batch;
+}
+
+["a", "b"].map(function (name) {
+    return name + "!";
+});
