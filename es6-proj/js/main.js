9b469fb8303dc701758f75e1b2c2a7adeb303d1b
Surya Kant Bansal <skb1129@yahoo.com>
2017-10-29 14:13:11 +0530

    updation

--- es6-proj/js/main.js
+++ es6-proj/js/main.js
@@ -1,57 +1,58 @@
-/*jshint esversion: 6*/
-
-let students = [
-	{
-		name: 'Surya Kant',
-		rno: '1510991666',
-		year: '2019',
-		stream: 'CSE'
-	},
-	{
-		name: 'Sparsh Rana',
-		rno: '1510991658',
-		year: '2019',
-		stream: 'CSE'
-	}
+let students = [{
+        name: 'Surya Kant',
+        rno: '1510991666',
+        year: '2019',
+        stream: 'CSE'
+    },
+    {
+        name: 'Sparsh Rana',
+        rno: '1510991658',
+        year: '2019',
+        stream: 'CSE'
+    }
 ];
 
 
-function createRow(student){
-	$('tbody').append(`<tr>
+function createRow(student) {
+    $('tbody').append(`<tr>
 						<td>` + student.name + `</td>
 						<td>` + student.rno + `</td>
 						<td>` + student.year + `</td>
 						<td>` + student.stream + `</td>
-						<td><a href="#" onclick="deleteRow(this)">delete</a>
-						<span> </span>
-						<a href="#" onclick="editRow()">edit</a></td>
+						<td><a href="#" onclick="deleteRow($(this))">
+						<i class="fa fa-trash-o fa-lg" aria-hidden="true"></i></a>
+						<span> | </span>
+						<a href="#" onclick="editRow()">
+						<i class="fa fa-pencil fa-lg" aria-hidden="true"></i></a></td>
 						</tr>`)
 }
 
 
-function initData(){
-	for(i=0; i < students.length; i++){
-		createRow(students[i]);
-	}
+function initData() {
+    for (i = 0; i < students.length; i++) {
+        createRow(students[i]);
+    }
 }
 
 
-function addData(){
-	let data = {};
-	$.each($('#myform').serializeArray(), function(i, field) {
-	    data[field.name] = field.value;
+function addData() {
+    let data = {};
+	let input = $('#myform').serializeArray();
+	$.each(input,function(index, object) {
+		data[object.name] = object.value;
 	});
+	students.push(data);
 	createRow(data);
 	$('#myform')[0].reset();
 }
 
 
-function deleteRow(o) {
-	let p = o.parentNode.parentNode;
-	p.parentNode.removeChild(p);
+function deleteRow(element) {
+    let tr = element.parent().parent();
+    tr.remove();
 }
 
 
-function editRow(){
+function editRow() {
 
 }
