baa7c4ea263546c32eede307fdd6ad1baa2849f8
Surya Kant Bansal <skb1129@yahoo.com>
2017-10-31 23:03:14 +0530

    live page update

--- docs/js/main.js
+++ docs/js/main.js
@@ -37,7 +37,7 @@ function createRow(student) {
 						<td><a href="#" onclick="deleteRow($(this))">
 						<i class="fa fa-trash-o fa-lg" aria-hidden="true"></i></a>
 						<span> | </span>
-						<a href="#" onclick="editRow()">
+						<a href="#" onclick="editRow($(this))">
 						<i class="fa fa-pencil fa-lg" aria-hidden="true"></i></a>
 						<span> | </span>
 						<a href="#" onclick="selectRow($(this))">
@@ -65,9 +65,16 @@ function addData() {
 	$.each($input,function(index, $object) {
 		data[$object.name] = $object.value;
 	});
+	if(checkEmpty(data) || checkUnique(data))
+		return;
 	students.push(data);
 	createRow(data);
 	$('#myform')[0].reset();
+	swal(
+		'Added!',
+		'The entry has been added!',
+		'success'
+	)
 }
 
 
@@ -75,16 +82,82 @@ function addData() {
  * It deletes a row form table and that row data from students array.
  */
 function deleteRow($element) {
-    let $tr = $element.parent().parent();
-	let rno = $tr.children('td')[1].textContent;
-	$tr.remove();
-	let index = students.findIndex(student => student.rno == rno);
-	students.splice(index,1);
+	if($element.prevObject){
+		let $tr = $element.parent().parent();
+		let rno = $tr.children('td')[1].textContent;
+		$tr.remove();
+		let index = students.findIndex(student => student.rno == rno);
+		students.splice(index,1);
+	}
+	else{
+		swal({
+			title: 'Are you sure?',
+			text: 'You are about to delete an entry!',
+			type: 'warning',
+			showCancelButton: true,
+			confirmButtonColor: '#3085d6',
+			cancelButtonColor: '#d33',
+			confirmButtonText: 'Delete!'
+			}).then(function () {
+				let $tr = $element.parent().parent();
+				let rno = $tr.children('td')[1].textContent;
+				$tr.remove();
+				let index = students.findIndex(student => student.rno == rno);
+				students.splice(index,1);
+			}).catch(swal.noop)
+	}
 }
 
 
-function editRow() {
-
+/* This function uses a form inside the swal window
+ * to edit the entries in the table.
+ */
+function editRow($element) {
+	let $tr = $element.parent().parent();
+	let name = $tr.children('td')[0].textContent;
+	let rno = $tr.children('td')[1].textContent;
+	let year = $tr.children('td')[2].textContent;
+	let stream = $tr.children('td')[3].textContent;
+	swal({
+		title: 'Edit Student Details :',
+		html:`
+			<form id="edit-form">
+			<input type="text" class="swal2-input" name="name"
+				placeholder="Name" value="${name}">
+			<input type="text" class="swal2-input" name="rno"
+				placeholder="Roll No." value="${rno}">
+			<input type="text" class="swal2-input" name="year"
+				placeholder="Passout Year" value="${year}">
+			<input type="text" class="swal2-input" name="stream"
+				placeholder="Stream" value="${stream}">
+			</form>`,
+		showCancelButton: true,
+		confirmButtonColor: '#3085d6',
+		cancelButtonColor: '#d33',
+		confirmButtonText: 'Update!'
+		}).then(function () {
+			let data = {};
+			let $input = $('#edit-form').serializeArray();
+			$.each($input,function(index, $object) {
+				data[$object.name] = $object.value;
+			});
+			if(checkEmpty(data))
+				return;
+			if(data[rno] == rno)
+				if(checkUnique(data))
+					return;
+			let index = students.findIndex(student => student.rno == rno);
+			students[index] = data;
+			$tr.children('td:nth-child(1)').html(data.name);
+			$tr.children('td:nth-child(2)').html(data.rno);
+			$tr.children('td:nth-child(3)').html(data.year);
+			$tr.children('td:nth-child(4)').html(data.stream);
+			swal(
+				'Entry Updated!',
+				'One entry has been successfully updated',
+				'success'
+			)
+		}).catch(swal.noop)
 }
 
 
@@ -123,7 +196,7 @@ function deleteSelected() {
 				`${$tr.length} entries deleted!`,
 				'success'
 			)
-		})
+		}).catch(swal.noop)
 }
 
 
@@ -137,4 +210,39 @@ function toggleDelBtn() {
 	else {
 		$('#delbtn').attr('hidden', 'true');
 	}
+}
+
+
+/* This function checks for empty fields and
+ * displays an alert if found.
+ * Use of ES6 for...of loop.
+ */
+function checkEmpty(data){
+	let pairs = Object.entries(data);
+	for(let pair of pairs)
+		if(pair[1] == ''){
+			swal(
+				'Empty fields found!',
+				'There must be no empty fields.',
+				'error'
+			)
+			return true;
+		}
+	return false;
+}
+
+
+/* This function checks if data entry is unique
+ * or not and displays alert if it isn't.
+ */
+function checkUnique(data){
+	if(students.find(student => student.rno == data.rno)){
+		swal(
+			'Unable to add!',
+			'Roll No. already exists.',
+			'error'
+		)
+		return true;
+	}
+	return false;
 }
