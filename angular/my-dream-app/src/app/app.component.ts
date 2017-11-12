2143ecd061fd9cd21f1ecaccbbbbd89a964e2a1d
Surya Kant Bansal <skb1129@yahoo.com>
2017-11-12 12:08:16 +0530

    messing with angular

--- angular/my-dream-app/src/app/app.component.ts
+++ angular/my-dream-app/src/app/app.component.ts
@@ -8,4 +8,12 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'app';
   batch = 'UCA-2';
+  factorial = this.fact(10);
+  fact(n: number) {
+    let res = 1;
+    for (let i = 1; i <= n; i++) {
+      res *= i;
+    }
+    return res;
+  }
 }
