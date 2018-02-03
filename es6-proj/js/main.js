3c90fbcd638695968b7ddbe8b35227c6b547d5d6
Surya Kant Bansal <skb1129@yahoo.com>
2018-02-03 09:55:03 +0530

    style: formatting and missing semicolons

--- es6-proj/js/main.js
+++ es6-proj/js/main.js
@@ -1,15 +1,15 @@
 let students = [{
-        name: 'Surya Kant',
-        rno: '1510991666',
-        year: '2019',
-        stream: 'CSE'
-    },
-    {
-        name: 'Sparsh Rana',
-        rno: '1510991658',
-        year: '2019',
-        stream: 'CSE'
-    },
+		name: 'Surya Kant',
+		rno: '1510991666',
+		year: '2019',
+		stream: 'CSE'
+	},
+	{
+		name: 'Sparsh Rana',
+		rno: '1510991658',
+		year: '2019',
+		stream: 'CSE'
+	},
 	{
 		name: 'Shivang',
 		rno: '1510991614',
@@ -29,22 +29,22 @@ let students = [{
  * It creates the row in the HTML DOM.
  */
 function createRow(student) {
-    $('tbody').append(`<tr>
-						<td scope="row"></td>
-						<td>${student.name}</td>
-						<td>${student.rno}</td>
-						<td>${student.year}</td>
-						<td>${student.stream}</td>
-						<td><a href="#" onclick="deleteRow($(this))">
-						<i class="fa fa-trash-o fa-lg" aria-hidden="true"></i></a>
-						<span> | </span>
-						<a href="#" onclick="editRow($(this))">
-						<i class="fa fa-pencil fa-lg" aria-hidden="true"></i></a>
-						<span> | </span>
-						<a href="#" onclick="selectRow($(this))">
-						<i class="fa fa-check fa-lg" aria-hidden="true"></i></a>
-						</td>
-						</tr>`)
+	$('tbody').append(`<tr>
+					<td scope="row"></td>
+					<td>${student.name}</td>
+					<td>${student.rno}</td>
+					<td>${student.year}</td>
+					<td>${student.stream}</td>
+					<td><a href="#" onclick="deleteRow($(this))">
+					<i class="fa fa-trash-o fa-lg" aria-hidden="true"></i></a>
+					<span> | </span>
+					<a href="#" onclick="editRow($(this))">
+					<i class="fa fa-pencil fa-lg" aria-hidden="true"></i></a>
+					<span> | </span>
+					<a href="#" onclick="selectRow($(this))">
+					<i class="fa fa-check fa-lg" aria-hidden="true"></i></a>
+					</td>
+					</tr>`);
 }
 
 
@@ -52,8 +52,8 @@ function createRow(student) {
  * students data to createRow() function.
  */
 function initData() {
-    for(let student of students)
-        createRow(student);
+	for (let student of students)
+		createRow(student);
 	serialize();
 }
 
@@ -61,16 +61,16 @@ function initData() {
 /* This function gets user input and creates a data entry.
  */
 function addData() {
-    let data = {};
+	let data = {};
 	let $input = $('#myform').serializeArray();
-	$.each($input,function(index, $object) {
+	$.each($input, function (index, $object) {
 		data[$object.name] = $object.value;
 	});
-	data.name = data.name.trim()
-	data.rno = data.rno.trim()
-	data.year = data.year.trim()
-	data.stream = data.stream.trim()
-	if(checkEmpty(data) || validate(data))
+	data.name = data.name.trim();
+	data.rno = data.rno.trim();
+	data.year = data.year.trim();
+	data.stream = data.stream.trim();
+	if (checkEmpty(data) || validate(data))
 		return;
 	students.push(data);
 	createRow(data);
@@ -80,7 +80,7 @@ function addData() {
 		'Added!',
 		'The entry has been added!',
 		'success'
-	)
+	);
 }
 
 
@@ -88,14 +88,13 @@ function addData() {
  * It deletes a row form table and that row data from students array.
  */
 function deleteRow($element) {
-	if($element.prevObject){
+	if ($element.prevObject) {
 		let $tr = $element.parent().parent();
 		let rno = $tr.children('td')[1].textContent;
 		$tr.remove();
 		let index = students.findIndex(student => student.rno == rno);
-		students.splice(index,1);
-	}
-	else{
+		students.splice(index, 1);
+	} else {
 		swal({
 			title: 'Are you sure?',
 			text: 'You are about to delete an entry!',
@@ -104,14 +103,14 @@ function deleteRow($element) {
 			confirmButtonColor: '#3085d6',
 			cancelButtonColor: '#d33',
 			confirmButtonText: 'Delete!'
-			}).then(function () {
-				let $tr = $element.parent().parent();
-				let rno = $tr.children('td')[1].textContent;
-				$tr.remove();
-				let index = students.findIndex(student => student.rno == rno);
-				students.splice(index,1);
-				serialize();
-			}).catch(swal.noop)
+		}).then(function () {
+			let $tr = $element.parent().parent();
+			let rno = $tr.children('td')[1].textContent;
+			$tr.remove();
+			let index = students.findIndex(student => student.rno == rno);
+			students.splice(index, 1);
+			serialize();
+		}).catch(swal.noop);
 	}
 }
 
@@ -127,44 +126,44 @@ function editRow($element) {
 	let stream = $tr.children('td')[4].textContent;
 	swal({
 		title: 'Edit Student Details :',
-		html:`
-			<form id="edit-form">
-				<input type="text" class="swal2-input" name="name"
-					placeholder="Name" value="${name}">
-				<input type="text" class="swal2-input" name="rno"
-					placeholder="Roll No." value="${rno}">
-				<input type="text" class="swal2-input" name="year"
-					placeholder="Passout Year" value="${year}">
-				<input type="text" class="swal2-input" name="stream"
-					placeholder="Stream" value="${stream}">
-			</form>`,
+		html: `
+		<form id="edit-form">
+			<input type="text" class="swal2-input" name="name"
+				placeholder="Name" value="${name}">
+			<input type="text" class="swal2-input" name="rno"
+				placeholder="Roll No." value="${rno}">
+			<input type="text" class="swal2-input" name="year"
+				placeholder="Passout Year" value="${year}">
+			<input type="text" class="swal2-input" name="stream"
+				placeholder="Stream" value="${stream}">
+		</form>`,
 		showCancelButton: true,
 		confirmButtonColor: '#3085d6',
 		cancelButtonColor: '#d33',
 		confirmButtonText: 'Update!'
-		}).then(function () {
-			let data = {};
-			let $input = $('#edit-form').serializeArray();
-			$.each($input,function(index, $object) {
-				data[$object.name] = $object.value;
-			});
-			if(checkEmpty(data))
+	}).then(function () {
+		let data = {};
+		let $input = $('#edit-form').serializeArray();
+		$.each($input, function (index, $object) {
+			data[$object.name] = $object.value;
+		});
+		if (checkEmpty(data))
+			return;
+		if (data[rno] == rno)
+			if (validate(data))
 				return;
-			if(data[rno] == rno)
-				if(validate(data))
-					return;
-			let index = students.findIndex(student => student.rno == rno);
-			students[index] = data;
-			$tr.children('td:nth-child(2)').html(data.name);
-			$tr.children('td:nth-child(3)').html(data.rno);
-			$tr.children('td:nth-child(4)').html(data.year);
-			$tr.children('td:nth-child(5)').html(data.stream);
-			swal(
-				'Entry Updated!',
-				'One entry has been successfully updated',
-				'success'
-			)
-		}).catch(swal.noop)
+		let index = students.findIndex(student => student.rno == rno);
+		students[index] = data;
+		$tr.children('td:nth-child(2)').html(data.name);
+		$tr.children('td:nth-child(3)').html(data.rno);
+		$tr.children('td:nth-child(4)').html(data.year);
+		$tr.children('td:nth-child(5)').html(data.stream);
+		swal(
+			'Entry Updated!',
+			'One entry has been successfully updated',
+			'success'
+		);
+	}).catch(swal.noop);
 }
 
 
@@ -193,18 +192,18 @@ function deleteSelected() {
 		confirmButtonColor: '#3085d6',
 		cancelButtonColor: '#d33',
 		confirmButtonText: 'Confirm!'
-		}).then(function () {
-			$tr.each(function(index) {
-				deleteRow($(this).children().children('a:first-child'));
-			});
-			toggleDelBtn();
-			serialize();
-			swal(
-				'Deleted!',
-				`${$tr.length} entries deleted!`,
-				'success'
-			)
-		}).catch(swal.noop)
+	}).then(function () {
+		$tr.each(function (index) {
+			deleteRow($(this).children().children('a:first-child'));
+		});
+		toggleDelBtn();
+		serialize();
+		swal(
+			'Deleted!',
+			`${$tr.length} entries deleted!`,
+			'success'
+		);
+	}).catch(swal.noop);
 }
 
 
@@ -212,10 +211,9 @@ function deleteSelected() {
  * of delete button.
  */
 function toggleDelBtn() {
-	if($('tr.table-danger').length) {
+	if ($('tr.table-danger').length) {
 		$('#delbtn').removeAttr('disabled');
-	}
-	else {
+	} else {
 		$('#delbtn').attr('disabled', 'true');
 	}
 }
@@ -225,15 +223,15 @@ function toggleDelBtn() {
  * displays an alert if found.
  * Use of ES6 for...of loop.
  */
-function checkEmpty(data){
+function checkEmpty(data) {
 	let pairs = Object.entries(data);
-	for(let pair of pairs)
-		if(pair[1] == ''){
+	for (let pair of pairs)
+		if (pair[1] == '') {
 			swal(
 				'Empty fields found!',
 				'There must be no empty fields.',
 				'error'
-			)
+			);
 			return true;
 		}
 	return false;
@@ -243,31 +241,29 @@ function checkEmpty(data){
 /* This function checks if data entry is unique and valid
  * and displays alert if it isn't.
  */
-function validate(data){
+function validate(data) {
 	let regex = /^[a-zA-Z .]{2,20}$/;
-	if(!data.name.match(regex) || !data.stream.match(regex)){
+	if (!data.name.match(regex) || !data.stream.match(regex)) {
 		swal(
 			'Unable to add!',
 			'Invalid Name and/or Stream.',
 			'error'
-		)
+		);
 		return true;
-	}
-	else if(isNaN(data.rno) || isNaN(data.year) ||
-	(parseInt(data.year) > 2030 || parseInt(data.year) < 1990)){
+	} else if (isNaN(data.rno) || isNaN(data.year) ||
+		(parseInt(data.year) > 2030 || parseInt(data.year) < 1990)) {
 		swal(
 			'Unable to add!',
 			'Invalid Roll No. and/or Year.',
 			'error'
-		)
+		);
 		return true;
-	}
-	else if(students.find(student => student.rno == data.rno)){
+	} else if (students.find(student => student.rno == data.rno)) {
 		swal(
 			'Unable to add!',
 			'Roll No. already exists.',
 			'error'
-		)
+		);
 		return true;
 	}
 	return false;
@@ -279,7 +275,7 @@ function validate(data){
  */
 function serialize() {
 	let $tr = $('tbody tr');
-	$tr.each(function(index) {
-		$(this).children('td:first-child').html(index+1);
+	$tr.each(function (index) {
+		$(this).children('td:first-child').html(index + 1);
 	});
 }
